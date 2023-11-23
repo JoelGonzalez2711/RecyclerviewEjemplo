0e1ba9f77d41eb965b70479ecccf778a29034a6a
JoelGonzalez2711 <joelgonzalez.alu@iespacomolla.es>
2023-11-23 14:31:47 +0100

    no va

--- app/src/main/java/com/example/recyclerviewejemplo/Superhero.kt
+++ app/src/main/java/com/example/recyclerviewejemplo/Superhero.kt
@@ -6,3 +6,8 @@ data class Superhero (
     var realName:String,
     var photo:String
 )
+
+val batman:Superhero = Superhero("Batman", "DC", "Bruce Wayne", "https://cursokotlin.com/wp-content/uploads/2017/07/batman.jpg")
+val batmanRealName = batman.realName //Recuperamos "Bruce Wayne"
+batman.realName = "Soy Batman" //Cambiamos "Brece Wayne" por "Soy Batman"
+batman.toString() //Devolverá todos los atributos con su valor
